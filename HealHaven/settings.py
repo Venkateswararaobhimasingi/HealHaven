e508fd23a799f175f787e952b5f931c56f634aa4
Venkateswararaobhimasingi <srinu19773@gmail.com>
2025-01-05 15:21:10 +0530

    ch17

--- HealHaven/settings.py
+++ HealHaven/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-8g8d-)4n@-sbi!*a!&haf96+4cnk3&ewsl&utq$zu%3xh-tz!s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
