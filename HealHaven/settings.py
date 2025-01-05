1b2fc5ba1b36714bd124146b395fd9e0810a8073
Venkateswararaobhimasingi <srinu19773@gmail.com>
2025-01-05 15:57:18 +0530

    ch19

--- HealHaven/settings.py
+++ HealHaven/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-8g8d-)4n@-sbi!*a!&haf96+4cnk3&ewsl&utq$zu%3xh-tz!s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -129,12 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Ensure this directory exists
+    os.path.join(BASE_DIR, 'blog', 'static'),  # Correct path to your static folder
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
 
