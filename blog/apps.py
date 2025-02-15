d9482f898ecf8303b81710a976d081d691e7d330
Venkateswararaobhimasingi <srinu19773@gmail.com>
2025-02-16 00:15:09 +0530

    notwantasp

--- blog/apps.py
+++ blog/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "blog"
-
-    def ready(self):
-        from .scheduler import start  # Import start function
-        start()
